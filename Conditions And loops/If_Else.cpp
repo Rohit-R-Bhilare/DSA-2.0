fe6d5617d7af3016f3d058747ff9927e49814aca
Rohit-R-Bhilare <136981459+Rohit-R-Bhilare@users.noreply.github.com>
2023-08-28 22:07:46 +0530

    Add files via upload

---
+++ Conditions And loops/If_Else.cpp
@@ -0,0 +1,17 @@
+// If Else Condition
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int year = 20;
+
+    if (year >= 20)
+    {
+        cout<<"You are eligible for Driving Lisence.";
+    }
+    else{
+        cout<<"You are not eligible";
+    }
+    return 0;
+}
